db4a7c38de04e714a4132f138b68b7ba25100bac
David Hu <davidhu91@gmail.com>
2010-09-05 23:53:51 -0400

    first modify starter. sends out fleets == num planets

--- mybot/MyBot.cc
+++ mybot/MyBot.cc
@@ -1,5 +1,47 @@
 #include <iostream>
+#include <algorithm>
 #include "PlanetWars.h"
+using namespace std;
+
+
+
+// Variables starting with capitals denote constants.
+
+
+//------------------------------------------------------------------------------
+// Classes
+//------------------------------------------------------------------------------
+
+// Planet Wars turn-specific variables. We cache since some getters are time
+// consuming O(n) ops.
+class PwState {
+ public:
+  PwState(const PlanetWars& pw);
+  vector <Fleet> MyFleets;
+  int MyFleetsSize;
+  vector <Planet> MyPlanets;
+  int MyPlanetsSize;
+};
+
+PwState::PwState(const PlanetWars& pw) {
+  MyFleets = pw.MyFleets();
+  MyFleetsSize = MyFleets.size();
+  MyPlanets = pw.MyPlanets();
+  MyPlanetsSize = MyPlanets.size();
+}
+
+
+//------------------------------------------------------------------------------
+// Global Variables
+//------------------------------------------------------------------------------
+
+
+
+
+//------------------------------------------------------------------------------
+// Helper Functions
+//------------------------------------------------------------------------------
+
 
 // The DoTurn function is where your code goes. The PlanetWars object contains
 // the state of the game, including information about all planets and fleets
@@ -11,16 +53,21 @@
 // starting point, or you can throw it out entirely and replace it with your
 // own. Check out the tutorials and articles on the contest website at
 // http://www.ai-contest.com/resources.
-void DoTurn(const PlanetWars& pw) {
-  // (1) If we currently have a fleet in flight, just do nothing.
-  if (pw.MyFleets().size() >= 1) {
+void DoTurn(const PlanetWars& planetWars) {
+  // Cached pw
+  PwState pw(planetWars);
+
+  // (1) Do nothing if our fleets count exceeds our planets count
+  if (pw.MyFleetsSize >= pw.MyPlanetsSize) {
+  //if (pw.MyFleetsSize >= 1) {
     return;
   }
+
   // (2) Find my strongest planet.
   int source = -1;
   double source_score = -999999.0;
   int source_num_ships = 0;
-  std::vector<Planet> my_planets = pw.MyPlanets();
+  vector<Planet> my_planets = planetWars.MyPlanets();
   for (int i = 0; i < my_planets.size(); ++i) {
     const Planet& p = my_planets[i];
     double score = (double)p.NumShips();
@@ -33,7 +80,7 @@ void DoTurn(const PlanetWars& pw) {
   // (3) Find the weakest enemy or neutral planet.
   int dest = -1;
   double dest_score = -999999.0;
-  std::vector<Planet> not_my_planets = pw.NotMyPlanets();
+  vector<Planet> not_my_planets = planetWars.NotMyPlanets();
   for (int i = 0; i < not_my_planets.size(); ++i) {
     const Planet& p = not_my_planets[i];
     double score = 1.0 / (1 + p.NumShips());
@@ -46,24 +93,24 @@ void DoTurn(const PlanetWars& pw) {
   // planet that I do not own.
   if (source >= 0 && dest >= 0) {
     int num_ships = source_num_ships / 2;
-    pw.IssueOrder(source, dest, num_ships);
+    planetWars.IssueOrder(source, dest, num_ships);
   }
 }
 
 // This is just the main game loop that takes care of communicating with the
 // game engine for you. You don't have to understand or change the code below.
 int main(int argc, char *argv[]) {
-  std::string current_line;
-  std::string map_data;
+  string current_line;
+  string map_data;
   while (true) {
-    int c = std::cin.get();
+    int c = cin.get();
     current_line += (char)c;
     if (c == '\n') {
       if (current_line.length() >= 2 && current_line.substr(0, 2) == "go") {
         PlanetWars pw(map_data);
         map_data = "";
         DoTurn(pw);
-	pw.FinishTurn();
+        pw.FinishTurn();
       } else {
         map_data += current_line;
       }
