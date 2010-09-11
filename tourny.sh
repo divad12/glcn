57dd118be796893147e0a13b9d23694aefb6bda6
David Hu <davidhu91@gmail.com>
2010-09-10 23:00:12 -0400

    tourny script

--- tourny.sh
+++ tourny.sh
@@ -2,6 +2,6 @@
 
 date >> results.txt
 
-./playall.sh ../bots/5/MyBot ../bots/4/MyBot ../bots/3/MyBot example_bots/BullyBot.jar example_bots/DualBot.jar example_bots/ProspectorBot.jar example_bots/RageBot.jar >> results.txt
+./playall.sh ../bots/5/MyBot ../bots/5-issueOrder-simple/MyBot ../bots/4/MyBot ../bots/4-no-fleets-limit/MyBot ../bots/3/MyBot example_bots/BullyBot.jar example_bots/DualBot.jar example_bots/ProspectorBot.jar example_bots/RageBot.jar >> results.txt
 
 echo >> results.txt
