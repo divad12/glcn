bc2b30f646dd6c7897aefe36374f112055830bd5
David Hu <davidhu91@gmail.com>
2010-09-11 23:55:29 -0400

    tournament results

--- tourny.sh
+++ tourny.sh
@@ -2,7 +2,8 @@
 
 date >> results.txt
 
-./playall.sh ../bots/6/MyBot ../bots/5/MyBot ../bots/5-skip-first-turn/MyBot ../bots/5-unlimited-fleets/MyBot ../bots/5-unlimited-fleets-of-10/MyBot  ../bots/4-no-fleets-limit/MyBot ../bots/3/MyBot example_bots/DualBot.jar example_bots/RageBot.jar >> results.txt
+./playall.sh ../bots/7/MyBot ../bots/7-frugally-take-ops/MyBot ../bots/6/MyBot ../bots/5/MyBot ../bots/5-skip-first-turn/MyBot ../bots/5-unlimited-fleets/MyBot ../bots/4-no-fleets-limit/MyBot ../bots/3/MyBot example_bots/DualBot.jar example_bots/RageBot.jar >> results.txt
 
+./playall.sh ../bots/7-frugally-take-ops/MyBot ../bots/7/MyBot ../bots/6/MyBot ../bots/5/MyBot ../bots/5-skip-first-turn/MyBot ../bots/5-unlimited-fleets/MyBot ../bots/4-no-fleets-limit/MyBot ../bots/3/MyBot example_bots/DualBot.jar example_bots/RageBot.jar >> results.txt
 
 echo >> results.txt
